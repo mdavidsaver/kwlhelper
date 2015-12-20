38bdc68d30eb6a833eaebcadf7237df12bf6e8ca
Michael Davidsaver <mdavidsaver@gmail.com>
2015-12-20 18:26:05 -0500

    cleanup

--- kwlexport.py
+++ kwlexport.py
@@ -1,129 +1,145 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+Export an entire kwallet in XML from file or stdout to facilitate backup
+and synchronization.
+
 Author: Michael Davidsaver <mdavidsaver@gmail.com>
-Copyright 2011
+Copyright 2015
 Licence: GPL 3+
 """
 
 appname='kwlexport'
 
-import codecs
 import sys
-from optparse import OptionParser
 import dbus
 import struct
 from base64 import standard_b64encode
 
 int32=struct.Struct('!I')
 
-opts=OptionParser(usage='%prog [-o file|-] [-w walletname]',
-                  description='Export KDE KWallet in XML form to stdout or file')
-opts.add_option('-o', '--output', help='Write output to file (- for stdout)')
-opts.add_option('-w', '--wallet', help='Wallet name (default is lexographic first)')
+def getargs():
+    from argparse import ArgumentParser
+    P = ArgumentParser(usage='%(prog)s [-o file|-] [-w walletname]',
+                       description='Export KDE KWallet in XML form to stdout or file')
+    P.add_argument('-o','--output', help='Write output to file (- for stdout)')
+    P.add_argument('-w', '--wallet', help='Wallet name (default is lexographic first)')
+    A = P.parse_args()
 
-opts, args = opts.parse_args()
-
-if opts.output or opts.output=='-':
-    out=codecs.open(opts.output, mode='w', encoding='utf-8')
-else:
-    out=sys.stdout
-
-sbus=dbus.SessionBus()
-proxy=sbus.get_object('org.kde.kwalletd', '/modules/kwalletd')
-kwallet=dbus.Interface(proxy, 'org.kde.KWallet')
-
-if opts.wallet:
-    wname=opts.wallet
-else:
-    x=kwallet.wallets()
-    if len(x)==0:
-        raise RuntimeError('No wallets found')
-    wname=str(x[0])
-
-thewallet=kwallet.open(wname, 0, appname)
-if thewallet<0:
-    raise RuntimeError('Failed to open wallet: '+wname)
-
-folders=kwallet.folderList(thewallet, appname)
-
-print >>out,'''<?xml version="1.0" encoding="UTF-8"?>
-<wallet name="%(name)s">'''%{'name':wname}
-
-for f in folders:
-    
-    ents=kwallet.entryList(thewallet,f,appname)
-
-    if len(ents)==0:
-        print >>out,u'    <folder name="%(name)s"/>'%{'name':f}
-        continue
-    
+    if A.output:
+        import codecs
+        A.output = codecs.open(A.output, mode='w', encoding='utf-8')
     else:
-        print >>out,u'    <folder name="%(name)s">'%{'name':f}
+        A.output = sys.stdout
 
-    for e in ents:
-        etype=kwallet.entryType(thewallet,f,e,appname)
-        
-        if etype==1:
-            p=kwallet.readPassword(thewallet,f,e,appname)
-            print >>out,' '*8,u'<password name="%s">%s</password>'%(e,p)
-            
-        elif etype==2:
-            p=kwallet.readEntry(thewallet,f,e,appname)
-            # returns a list of bytes
-            # convert to byte string
-            p=map(str,p)
-            if len(p)==0:
-                p=u''
-            else:
-                p=reduce(str.__add__, p)
-            p=standard_b64encode(p)
+    return A
 
-            print >>out,' '*8,u'<stream name="%s">%s</stream>'%(e,p)
-            
-        elif etype==3:
-            p=kwallet.readMap(thewallet,f,e,appname)
-            # returns a list of dbus.Byte
-            # convert to byte string
-            p=reduce(str.__add__, map(chr,p))
+def dopen():
+    sbus=dbus.SessionBus()
+    proxy=sbus.get_object('org.kde.kwalletd', '/modules/kwalletd')
+    return sbus, dbus.Interface(proxy, 'org.kde.KWallet')
 
-            # first 4 bytes is the number of pairs
-            count,=int32.unpack(p[:4])
-            p=p[4:]
+def main(kwallet, args):
+    out = args.output
 
-            if count==0:
-                print >>out,' '*8,u'<map name="%s"/>'%e
-                continue
-            
-            else:
-                print >>out,' '*8,u'<map name="%s">'%e
-                for n in range(count):
+    if args.wallet:
+        wname=args.wallet
+    else:
+        x=kwallet.wallets()
+        if len(x)==0:
+            raise RuntimeError('No wallets found')
+        wname=str(x[0])
 
-                    # length of payload in bytes
-                    keylen,=int32.unpack(p[:4])
-                    p=p[4:]
-                    key=p[:keylen].decode('utf-16be') # payload
-                    p=p[keylen:]
+    thewallet=kwallet.open(wname, 0, appname)
+    if thewallet<0:
+        raise RuntimeError('Failed to open wallet: '+wname)
 
-                    vallen,=int32.unpack(p[:4])
-                    p=p[4:]
-                    if vallen==0xffffffff:
-                        val = ''
-                    else:
-                        val=p[:vallen].decode('utf-16be')
-                        p=p[vallen:]
+    folders=kwallet.folderList(thewallet, appname)
 
-                    print >>out,' '*12,u'<mapentry name="%s">%s</mapentry>'%(key,val)
-                print >>out,' '*8,'</map>'
+    print >>out,'''<?xml version="1.0" encoding="UTF-8"?>
+    <wallet name="%(name)s">'''%{'name':wname}
+
+    for f in folders:
+
+        ents=kwallet.entryList(thewallet,f,appname)
+
+        if len(ents)==0:
+            print >>out,u'    <folder name="%(name)s"/>'%{'name':f}
+            continue
 
         else:
-            print >>sys.stderr,'Ignoring unknown entry type',etype
+            print >>out,u'    <folder name="%(name)s">'%{'name':f}
 
-    print >>out,'    </folder>'
+        for e in ents:
+            etype=kwallet.entryType(thewallet,f,e,appname)
 
-print >>out,'</wallet>'
+            if etype==1:
+                p=kwallet.readPassword(thewallet,f,e,appname)
+                print >>out,' '*8,u'<password name="%s">%s</password>'%(e,p)
 
-kwallet.close(thewallet,False,appname)
+            elif etype==2:
+                p=kwallet.readEntry(thewallet,f,e,appname)
+                # returns a list of bytes
+                # convert to byte string
+                p=map(str,p)
+                if len(p)==0:
+                    p=u''
+                else:
+                    p=reduce(str.__add__, p)
+                p=standard_b64encode(p)
 
-if out is not sys.stdout:
-    out.close()
+                print >>out,' '*8,u'<stream name="%s">%s</stream>'%(e,p)
+
+            elif etype==3:
+                p=kwallet.readMap(thewallet,f,e,appname)
+                # returns a list of dbus.Byte
+                # convert to byte string
+                p=reduce(str.__add__, map(chr,p))
+
+                # first 4 bytes is the number of pairs
+                count,=int32.unpack(p[:4])
+                p=p[4:]
+
+                if count==0:
+                    print >>out,' '*8,u'<map name="%s"/>'%e
+                    continue
+
+                else:
+                    print >>out,' '*8,u'<map name="%s">'%e
+                    for n in range(count):
+
+                        # length of payload in bytes
+                        keylen,=int32.unpack(p[:4])
+                        p=p[4:]
+                        key=p[:keylen].decode('utf-16be') # payload
+                        p=p[keylen:]
+
+                        vallen,=int32.unpack(p[:4])
+                        p=p[4:]
+                        if vallen==0xffffffff:
+                            val = ''
+                        else:
+                            val=p[:vallen].decode('utf-16be')
+                            p=p[vallen:]
+
+                        print >>out,' '*12,u'<mapentry name="%s">%s</mapentry>'%(key,val)
+                    print >>out,' '*8,'</map>'
+
+            else:
+                print >>sys.stderr,'Ignoring unknown entry type',etype
+
+        print >>out,'    </folder>'
+
+    print >>out,'</wallet>'
+
+    kwallet.close(thewallet,False,appname)
+
+if __name__=='__main__':
+    session, kwallet = dopen()
+    args = getargs()
+    try:
+        main(kwallet, args)
+    finally:
+        session.close()
+        args.output.close()
+
--- kwlimport.py
+++ kwlimport.py
@@ -10,106 +10,118 @@ appname='kwlimport'
 
 import codecs
 import sys
-from optparse import OptionParser
 import dbus
 import struct
 from base64 import standard_b64decode
 
-from xml.etree.ElementTree import fromstring
-
 int32=struct.Struct('!I')
 
-opts=OptionParser(usage='%prog [-i file|-] [-w walletname] [-O]',
+def getargs():
+    from argparse import ArgumentParser
+    P = ArgumentParser(usage='%(prog)s [-i file|-] [-w walletname] [-O]',
                   description='Import KDE KWallet in XML form from stdin or file')
-opts.add_option('-i', '--input', help='Read output to file (- for stdout)')
-opts.add_option('-w', '--wallet', help='Wallet name (default is lexographic first)')
-opts.add_option('-O', '--overwrite', help='Overwrite existing entries', action='store_true',
-                default=False)
+    P.add_argument('-i', '--input', help='Read output to file (- for stdout)')
+    P.add_argument('-w', '--wallet', help='Wallet name (override name in file)')
+    P.add_argument('-O', '--overwrite', help='Overwrite existing entries',
+                 action='store_true', default=False)
+    A = P.parse_args()
 
-opts, args = opts.parse_args()
+    if A.input:
+        A.input = codecs.open(A.input, mode='r', encoding='utf-8')
+    else:
+        A.input = sys.stdin
+    return A
 
-if opts.input or opts.input=='-':
-    inp=codecs.open(opts.input, mode='r', encoding='utf-8')
-else:
-    inp=sys.stdin
-    
-data=fromstring(inp.read())
+def dopen():
+    sbus=dbus.SessionBus()
+    proxy=sbus.get_object('org.kde.kwalletd', '/modules/kwalletd')
+    return sbus, dbus.Interface(proxy, 'org.kde.KWallet')
 
-sbus=dbus.SessionBus()
-proxy=sbus.get_object('org.kde.kwalletd', '/modules/kwalletd')
-kwallet=dbus.Interface(proxy, 'org.kde.KWallet')
+def main(kwallet, args):
+    from xml.etree.ElementTree import fromstring
 
-if opts.wallet:
-    wname=opts.wallet
-else:
-    x=kwallet.wallets()
-    if len(x)==0:
-        raise RuntimeError('No wallets found')
-    wname=str(x[0])
+    data=fromstring(args.input.read())
 
-thewallet=kwallet.open(wname, 0, appname)
-if thewallet<0:
-    raise RuntimeError('Failed to open wallet: '+wname)
+    if args.wallet:
+        wname=args.wallet
+    else:
+        wname=data.attrib('name')
 
-folders=kwallet.folderList(thewallet, appname)
+    thewallet=kwallet.open(wname, 0, appname)
+    if thewallet<0:
+        raise RuntimeError('Failed to open wallet: '+wname)
 
-for f in data.findall('folder'):
-    fn=f.attrib['name']
-    if fn not in folders:
-        print 'create folder',fn
-        kwallet.createFolder(thewallet, fn, appname)
+    folders=kwallet.folderList(thewallet, appname)
 
-    ents=kwallet.entryList(thewallet,fn,appname)
-    
-    for e in f.findall('password'):
-        en=e.attrib['name']
-        val=e.text
-        
-        if en in ents and not opts.overwrite:
-            print 'Skipping password:',en
-            continue
+    for f in data.findall('folder'):
+        fn=f.attrib['name']
+        if fn not in folders:
+            print 'create folder',fn
+            kwallet.createFolder(thewallet, fn, appname)
 
-        kwallet.writePassword(thewallet,fn,en,val,appname)
-        print 'Set password',en #,'with',val
+        ents=kwallet.entryList(thewallet,fn,appname)
 
-    for e in f.findall('map'):
-        en=e.attrib['name']
-        
-        bytestr=''
-        count=0
+        for e in f.findall('password'):
+            en=e.attrib['name']
+            val=e.text
 
-        if en in ents and not opts.overwrite:
-            print 'Skipping map:',en
-            continue
+            if en in ents and not args.overwrite:
+                print 'Skipping password:',en
+                continue
 
-        for me in e.findall('mapentry'):
-            keystr=me.attrib['name'].encode('utf-16be')
-            valstr=me.text.encode('utf-16be')
+            kwallet.writePassword(thewallet,fn,en,val,appname)
+            print 'Set password',en #,'with',val
 
-            bytestr+='%s%s%s%s'%(int32.pack(len(keystr)),
-                                 keystr,
-                                 int32.pack(len(valstr)),
-                                 valstr)
-                                 
-            count+=1
+        for e in f.findall('map'):
+            en=e.attrib['name']
 
-        bytestr=int32.pack(count)+bytestr
+            bytestr=''
+            count=0
 
-        kwallet.writeMap(thewallet,fn,en,bytestr,appname)
-        print 'Set map',en #,'with',repr(bytestr)
+            if en in ents and not args.overwrite:
+                print 'Skipping map:',en
+                continue
 
+            for me in e.findall('mapentry'):
+                keystr=me.attrib['name'].encode('utf-16be')
+                if me.text is None:
+                    valstr = ''
+                else:
+                    valstr=me.text.encode('utf-16be')
 
-    for e in f.findall('stream'):
-        en=e.attrib['name']
+                bytestr+='%s%s%s%s'%(int32.pack(len(keystr)),
+                                     keystr,
+                                     int32.pack(len(valstr)),
+                                     valstr)
 
-        val=standard_b64decode(e.text)
+                count+=1
 
-        if en in ents and not opts.overwrite:
-            print 'Skipping binary:',en
-            continue
+            bytestr=int32.pack(count)+bytestr
 
-        kwallet.writeEntry(thewallet,fn,en,val,appname)
-        print 'Set binary',en #,'with',val
+            kwallet.writeMap(thewallet,fn,en,bytestr,appname)
+            print 'Set map',en #,'with',repr(bytestr)
 
 
-kwallet.close(thewallet,False,appname)
+        for e in f.findall('stream'):
+            en=e.attrib['name']
+
+            val=standard_b64decode(e.text)
+
+            if en in ents and not args.overwrite:
+                print 'Skipping binary:',en
+                continue
+
+            kwallet.writeEntry(thewallet,fn,en,val,appname)
+            print 'Set binary',en #,'with',val
+
+
+    kwallet.close(thewallet,False,appname)
+
+if __name__=='__main__':
+    session, kwallet = dopen()
+    args = getargs()
+    try:
+        main(kwallet, args)
+    finally:
+        session.close()
+        args.input.close()
